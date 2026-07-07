0ad4cdf5933fbb4d7d578c72331d1155de4f1991
eCommerce proyectos <equantumg@gmail.com>
2026-07-07 08:08:26 -0300

    Delete react-app directory

--- react-app/src/services/adminService.ts
+++
@@ -1,9 +0,0 @@
-import { delay } from './authService';
-import { mockAdminStats } from '../data/mockData';
-
-export const adminService = {
-  async getDashboardStats(): Promise<typeof mockAdminStats> {
-    await delay();
-    return mockAdminStats;
-  }
-};
